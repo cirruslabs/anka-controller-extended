2f0c54eba139cfdf8022644a62e7ac4d7188b100
fedor <fedor.korotkov@gmail.com>
2018-05-03 12:33:59 -0400

    More logs

--- impl/src/main/kotlin/org/cirruslabs/anka/controller/ControllerServiceImpl.kt
+++ impl/src/main/kotlin/org/cirruslabs/anka/controller/ControllerServiceImpl.kt
@@ -8,10 +8,12 @@ import org.cirruslabs.anka.sdk.AnkaVMManager
 class ControllerServiceImpl(val manager: AnkaVMManager) : ControllerGrpc.ControllerImplBase() {
   override fun startVM(request: StartVMRequest, responseObserver: StreamObserver<StartVMResponse>) {
     try {
+      println("Starting VM ${request.template}:${request.tag}...")
       val instanceId = manager.startVM(
         request.template,
         if (request.tag.isNullOrEmpty()) null else request.tag
       )
+      println("Started VM $instanceId from ${request.template}:${request.tag}!")
       val response = StartVMResponse.newBuilder()
         .setVmId(instanceId)
         .build()
@@ -23,31 +25,38 @@ class ControllerServiceImpl(val manager: AnkaVMManager) : ControllerGrpc.Control
         manager.execute(vm, request.script)
       }
     } catch (e: Exception) {
+      e.printStackTrace()
       responseObserver.onError(e)
     }
   }
 
   override fun stopVM(request: StopVMRequest, responseObserver: StreamObserver<StopVMResponse>) {
     try {
+      println("Stopping VM ${request.vmId}...")
       val response = StopVMResponse.newBuilder()
         .setSuccess(manager.stopVM(request.vmId))
         .build()
+      println("Stopped VM ${request.vmId}!")
       responseObserver.onNext(response)
       responseObserver.onCompleted()
     } catch (e: Exception) {
+      e.printStackTrace()
       responseObserver.onError(e)
     }
   }
 
   override fun vmStatus(request: VMStatusRequest, responseObserver: StreamObserver<VMStatusResponse>) {
     try {
+      println("Getting status for VM ${request.vmId}...")
       val status = manager.vmInfo(request.vmId)?.vmInfo?.status ?: "NotFound"
+      println("Status for VM ${request.vmId}: $status")
       val response = VMStatusResponse.newBuilder()
         .setStatus(status)
         .build()
       responseObserver.onNext(response)
       responseObserver.onCompleted()
     } catch (e: Exception) {
+      e.printStackTrace()
       responseObserver.onError(e)
     }
   }
