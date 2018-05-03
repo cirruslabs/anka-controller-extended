bf77fd0ad43aa1ccbeff39aa798bd27278f69659
fedor <fedor.korotkov@gmail.com>
2018-05-03 11:29:34 -0400

    fixed tun mode

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ -v ANYCONNECT_SERVER ]]; then
-  ip tuntap add vpn0 mode tun0
+  ip tuntap add vpn0 mode tun
 
   ( echo yes; echo $ANYCONNECT_PASSWORD ) | \
     openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp --background
