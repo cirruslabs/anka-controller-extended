d6997ffc2cb2980574de76230672ab33c56a7d8e
fedor <fedor.korotkov@gmail.com>
2018-05-03 19:02:43 -0400

    useful test

---
+++ client/src/test/kotlin/org/cirruslabs/anka/controller/ClientFactoryTest.kt
@@ -0,0 +1,24 @@
+package org.cirruslabs.anka.controller
+
+import io.grpc.ManagedChannelBuilder
+import org.cirruslabs.anka.controller.grpc.VMStatusRequest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * A useful test for checking connection to an Anka Controller Extended
+ */
+abstract class ClientFactoryTest {
+  @Test
+  fun testConnection() {
+    val channel = ManagedChannelBuilder.forTarget("IP:8239")
+      .usePlaintext(true)
+      .build()
+    val client = ClientFactory.create(channel)
+    val request = VMStatusRequest.newBuilder()
+      .setVmId("not-exists")
+      .build()
+
+    assertEquals("NotFound", client.vmStatus(request).get().status)
+  }
+}
