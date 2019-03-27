7db7867c6e28a2a2ea60c3c77f342649f3b28b64
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-03-27 15:34:01 -0400

    Check health via nodes

--- impl/src/main/kotlin/org/cirruslabs/anka/controller/health/ManagerHealthCheck.kt
+++ impl/src/main/kotlin/org/cirruslabs/anka/controller/health/ManagerHealthCheck.kt
@@ -6,7 +6,7 @@ import org.cirruslabs.anka.sdk.AnkaVMManager
 class ManagerHealthCheck(val manager: AnkaVMManager): HealthCheck() {
   override fun check(): Result {
     return try {
-      manager.communicator.listInstances()
+      manager.communicator.listNodes()
       Result.healthy("Healthy connection to Anka!")
     } catch (e: Exception) {
       Result.unhealthy(e)
